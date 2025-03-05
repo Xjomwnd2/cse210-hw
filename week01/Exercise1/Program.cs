78ee9131b60c602b463f4fa74e99b68958997abd
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-06 02:17:21 +0300

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,20 +11,6 @@ class Program
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-
     }
 }
+commit
