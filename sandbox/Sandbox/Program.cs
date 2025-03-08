fb9b685615c3361f1c092ed972a7853081f7d8d4
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-08 03:51:24 +0300

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Declare variables
+        string name;
+        int age;
+        double height;
+        
+        // Get user input
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+        
+        Console.Write("Enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter your height in meters: ");
+        height = Convert.ToDouble(Console.ReadLine());
+        
+        // Display output
+        Console.WriteLine("\nHello, " + name + "!");
+        Console.WriteLine("You are " + age + " years old.");
+        Console.WriteLine("You are " + height + " meters tall.");
+        
+        // Using string interpolation (more modern approach)
+        Console.WriteLine($"\nHello, {name}!");
+        Console.WriteLine($"You are {age} years old.");
+        Console.WriteLine($"You are {height} meters tall.");
+        
+        Console.ReadKey();
+
         Console.WriteLine("Hello World! This is the Sandbox Project.");
     }
 }
